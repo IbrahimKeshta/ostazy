7fa8e14c9856725492ca47069bfb932e8abc4a69
IbrahimKeshta <keshta95@hotmail.com>
2017-08-17 15:13:43 +0200

    update mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development' // only set on horuko
 
-if (env === 'development') {
+if (env === 'development' || env === 'test') {
     var config = require('./config.json');
     var envConfig = config[env]; // return an objects of the env
     // takes an object like envConfig return them as array
