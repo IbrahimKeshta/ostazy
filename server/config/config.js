a696ee95b11486165b1b6ac2165e9a4b2c769009
IbrahimKeshta <keshta95@hotmail.com>
2017-08-17 06:15:16 +0200

    testing

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development' // only set on horuko
 
-if (env === 'development' || env === 'test') {
+if (env === 'development') {
     var config = require('./config.json');
     var envConfig = config[env]; // return an objects of the env
     // takes an object like envConfig return them as array
