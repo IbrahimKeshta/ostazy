dcae725753586499a3a03c0ffb7a7a3f8bcf87eb
IbrahimKeshta <keshta95@hotmail.com>
2017-08-17 06:08:19 +0200

    some testing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect("mongodb://ibrahimkeshta:Ebrahim2525@ds149353.mlab.com:49353/ostazy_api");
 
 module.exports = {mongoose};
